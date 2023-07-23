64f13f9aa21e504daf45bdc1a75d658a75b00f79
Rksingh090 <54115599+Rksingh090@users.noreply.github.com>
2023-07-23 20:20:01 +0530

    dgd

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const express = require("express")
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "API / is running"
+    })
+})
+
+app.get("/test", (req, res) => {
+    res.json({
+        message: "API /test is running"
+    })
+})
+
+app.listen(4000, () => {
+    console.log("App running");
+})
+
+
