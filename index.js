55cb9adcf38a6ec9c57bdbf258e6f2d165d182b2
Rksingh090 <54115599+Rksingh090@users.noreply.github.com>
2023-08-01 09:51:36 +0530

    fg

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const express = require("express")
-
+const express = require("express");
 
 const app = express();
 
+function logger(req, res, next) {
+  const timestamp = new Date().toISOString();
+  const { method, url } = req;
+  console.log(`[${timestamp}] ${method} ${url}`);
+  next();
+}
+
+app.use(logger);
+
 app.get("/", (req, res) => {
-    res.json({
-        message: "API / is running"
-    })
-})
+  res.json({
+    message: "API / is running",
+  });
+});
 
 app.get("/test", (req, res) => {
-    res.json({
-        message: "Test Successfull!!!"
-    })
-})
+  res.json({
+    message: "Test Successfull!!!",
+  });
+});
 
 app.listen(4000, () => {
-    console.log("App running");
-})
-
-
+  console.log("App running");
+});
